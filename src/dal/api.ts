973784f7f86142e7de743172e99b130fe549295d
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-07-22 09:19:28 +0300

    first commit

--- src/dal/api.ts
+++ src/dal/api.ts
@@ -3,13 +3,13 @@ import axios from "axios";
 
 export const postApi = {
   getPosts(){
-    return axios.get<ResponseType>('https://jsonplaceholder.typicode.com/posts/')
+    return axios.get<Array<PostType>>('https://jsonplaceholder.typicode.com/posts/')
   },
   createPost(title: string, body: string, userId: number){
     return axios.post<PostType>('https://jsonplaceholder.typicode.com/posts', { title, body, userId})
   },
-  updatePost(id: number){
-    return axios.put<PostType>(`https://jsonplaceholder.typicode.com/posts/${id}`)
+  updatePost(id: number, title: string, body: string, userId: number){
+    return axios.put<PostType>(`https://jsonplaceholder.typicode.com/posts/${id}`, {title, body, userId})
   },
   deletePost(id: number){
     return axios.delete<PostType>(`https://jsonplaceholder.typicode.com/posts/${id}`)
@@ -17,12 +17,9 @@ export const postApi = {
 }
 
 //Type
-type PostType = {
+export type PostType = {
   id: number
   title: string
   body: string
   userId: number
-}
-type ResponseType = {
-  posts: Array<PostType>
 }
